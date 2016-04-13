cb4d80f94bd570ae414780607c1585c5262ae3cb
CaioMeloSilva <caio.cmsilva@gmail.com>
2016-04-12 22:39:41 -0300

    Create de resource controller

---
+++ Classes/Class11_Resource_Controller/control/ControlManager.php
@@ -0,0 +1,28 @@
+<?php
+
+include "RequestController.php";
+include "ResourceController.php";
+
+class ControlManager
+{
+	private $resourceController;
+	private $requestController;
+
+	public function __construct()
+	{
+		$this->resourceController = new ResourceController();
+
+		$this->requestController = new RequestController();
+	}
+
+	public function getResource()
+	{
+		$request = $this->requestController-> createRequest(
+     		    $_SERVER["SERVER_PROTOCOL"],
+			    $_SERVER["REQUEST_METHOD"],
+			    $_SERVER["REQUEST_URI"],
+			    $_SERVER["SERVER_ADDR"]);
+
+		return $this->resourceController->createResource($request);
+	}	
+}
---
+++ Classes/Class11_Resource_Controller/control/ResourceController.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once "model/Request.php";
+include_once "control/UserController.php";
+
+class ResourceController
+{
+
+	private $controlMap = 
+	[
+		"cebola" => "CebolaController",
+		"user" => "UserController",
+		"product" => "ProductController",
+	];
+
+	public function createResource($request)
+	{
+		return new $this->controlMap[$request->get_resource()]());
+	}
+}
